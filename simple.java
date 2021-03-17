87451af7e63a20c5014e2b6113c3497f3727b569
gowtham reddy <gowthamreddy1950@gmail.com>
2021-03-17 11:01:39 +0530

    changes made to april rel

--- simple.java
+++ simple.java
@@ -1,5 +1,8 @@
 class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
+    public static void main(String args[]){
+	    int a, b, c;
+     System.out.println("Hello Java");
+     systwm.out.println("This is my april-release code");
+
     }  
 }  
